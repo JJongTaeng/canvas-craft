513d56b21d5ca9bb58d9c251f0f1a2e818f7369c
조인태 <63296903+JJongTaeng@users.noreply.github.com>
2024-04-05 23:20:50 +0900

    [version] 0.0.4

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,5 @@
 export { DrawType } from './DrawType';
 export { Shape } from './Shape';
 export { CanvasCraft } from './CanvasCraft';
-export { AdvancedPath2D } from './AdvancedPath2D.ts';
-export { PathManager } from './PathManager.ts';
+export { AdvancedPath2D } from './AdvancedPath2D';
+export { PathManager } from './PathManager';
